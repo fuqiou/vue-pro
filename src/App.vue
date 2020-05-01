b7260603cbfb4d392c86043defb2392b1407b6b8
1661153225 <1661153225@qq.com>
2020-05-01 16:25:55 +0800

    fu-first

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    appvue
+    <p>{{ cmsg }}</p>
+    <!--3.使用子组件-->
+    <HelloWorld :msg="msg" @change="setCmsg"></HelloWorld>
+  </div>
+</template>
+
+<script>
+// 1.引入HelloWorld组件
+import HelloWorld from '@/components/HelloWorld' // @是映射，指向src，表示在src找，，，不像之前./要多层去找
+export default {
+  data () {
+    return {
+      msg: '我是父组件传过来的数据',
+      cmsg: ''
+    }
+  },
+  methods: {
+    setCmsg (cmsg) {
+      this.cmsg = cmsg
+    }
+  },
+  // 2.局部注册组件
+  components: {
+    // HelloWorld: HelloWorld可简写成下面的格式
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+  /* App.vue里面的样式是全局样式，会覆盖掉组件里面的样式 */
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    hello world
+    <p>{{ msg }}</p>
+    <p><button @click="toParent">点击给父组件传值</button></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      cmsg: '我是子组件传给父组件的数据'
+    }
+  },
+  props: {
+    msg: {
+      type: String
+    }
+  },
+  methods: {
+    toParent () {
+      // 触发绑定在自身上面的change方法
+      this.$emit('change', this.cmsg)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- 在style标签上面添加scoped属性可以让style里面的样式只作用于当前组件-->
+<!-- 可以在style标签上面添加一个lang='scss' sass-lader-->
+<!-- 有个组件内部未来不影响其他组件的样式，就在style标签添加scoped，后续-->
+<!-- /如果需要影响第三方插件和子组件样式的话，需要通过/deep/-->
+<style scoped lang="scss">
+
+</style>
